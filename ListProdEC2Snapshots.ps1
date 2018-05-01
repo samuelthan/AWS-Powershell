feec0616114187efd79dc166f4a6274f312b8f47
Samuel Than <samuelthan@gmail.com>
2018-05-01 21:10:13 +1000

    Initial

---
+++ ListProdEC2Snapshots.ps1
@@ -0,0 +1,4 @@
+﻿$ProfileName = Read-Host "Enter profile name to use"
+$AWSAccountNumber = Read-Host "Enter AWS Account Number"
+#Get-EC2Snapshot -ProfileName $ProfileName -OwnerId $AWSAccountNumber  | Select-Object SnapshotId, VolumeId, VolumeSize, StartTime, OwnerId, Description | Out-GridView
+Get-EC2Snapshot -ProfileName $ProfileName -OwnerId $AWSAccountNumber | Select-Object SnapshotId, VolumeId, VolumeSize, StartTime, OwnerId, Description |  Export-CSV -Path C:\Temp\Prod-EC2SnapShots.csv -Encoding ascii -NoTypeInformation
