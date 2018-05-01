89c174f1a6b6cb70d7ef18cd2648ea836e4cb188
Samuel Than <samuelthan@gmail.com>
2018-05-01 21:09:56 +1000

    Initial

---
+++ ListProdEC2Volumes.ps1
@@ -0,0 +1,28 @@
+﻿$ProfileName = Read-Host "Enter profile name to use"
+$Regions = (Get-AWSRegion).Region 
+$Volumes = foreach ($Region in $Regions){
+    Get-EC2Volume -ProfileName $ProfileName
+    }
+
+$VolumeDetailsOutArray = @()
+$myobj = @()
+foreach ($Volume in $Volumes){
+
+        $myobj = "" | Select "InstanceId","VolumeId","Size","SnapshotId","Name"
+        #Fill the object with the values mentioned above
+        $myobj.VolumeId = $Volume.VolumeId
+        $myobj.InstanceId = $Volume.Attachments.InstanceId
+        $myobj.Size = $Volume.Size
+        $myobj.SnapshotId = $Volume.SnapshotId
+        $myobj.Name = ($Volume.Tags | ? {$_.Key -EQ "Name"}).Value | Out-String -Stream
+        
+        #Add the objects to the Volume Out Arrays
+        $VolumeDetailsOutArray += $myobj
+
+        #Wipe the temp object just to be sure
+        $myobj = $null
+    }
+
+
+#$VolumeDetailsOutArray | Out-GridView
+$VolumeDetailsOutArray | Export-Csv C:\Temp\Prod-EC2Volumes.csv -Encoding ascii -NoTypeInformation
